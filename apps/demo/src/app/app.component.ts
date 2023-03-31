686293f64afd6872eec34d69a797822cfae66c13
Bence Hornyák <sir.bence15@gmail.com>
2023-03-31 21:36:14 +0200

    docs: prepare demo

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from "@angular/core";
-import { NxWelcomeComponent } from "./nx-welcome.component";
+import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxInputUppercaseifyDirective } from '@benceHornyak/ngx-input-uppercaseify';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
-  selector: "ngx-input-uppercaseify-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  selector: 'ngx-input-uppercaseify-root',
+  imports: [NgxInputUppercaseifyDirective, FormsModule, ReactiveFormsModule],
+  template: `
+    <div>
+      <label for="input-without-ngcontrol">Input without control</label>
+      <input id="input-without-ngcontrol" ngxInputUppercaseify />
+    </div>
+
+    <div>
+      <label for="input-with-ngmodel">Input with ngModel</label>
+      <input
+        id="input-with-ngmodel"
+        ngxInputUppercaseify
+        [(ngModel)]="ngModelValue"
+      />
+      <p>ngModel value: {{ ngModelValue }}</p>
+    </div>
+
+    <div>
+      <label for="input-with-formcontrol">Input with formControl</label>
+      <input
+        id="input-with-formcontrol"
+        ngxInputUppercaseify
+        [formControl]="formControl"
+      />
+      <p>formControl value: {{ formControl.value }}</p>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = "demo";
+  ngModelValue = '';
+  readonly formControl = new FormControl('');
 }
