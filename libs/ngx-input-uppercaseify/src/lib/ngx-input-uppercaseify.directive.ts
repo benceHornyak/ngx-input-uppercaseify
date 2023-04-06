71848512f482600bd6e42e6d3753ac0d46e1f516
Bence Hornyák <sir.bence15@gmail.com>
2023-04-06 21:47:11 +0200

    refactor(ngxUppercaseify): better uppercase implementation

--- libs/ngx-input-uppercaseify/src/lib/ngx-input-uppercaseify.directive.ts
+++ libs/ngx-input-uppercaseify/src/lib/ngx-input-uppercaseify.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Host, HostListener } from '@angular/core';
+import { isNil } from './typeguards/isNil.typeguard';
 
 @Directive({
   selector: 'input[type="text"][ngxInputUppercaseify]',
@@ -7,7 +8,40 @@ import { Directive, ElementRef, Host, HostListener } from '@angular/core';
 export class NgxInputUppercaseifyDirective {
   constructor(@Host() private inputElement: ElementRef<HTMLInputElement>) {}
 
-  @HostListener('input', ['$event.target.value']) onInput(value: string) {
-    this.inputElement.nativeElement.value = value.toUpperCase();
+  @HostListener('beforeinput', ['$event'])
+  onBeforeInput(event: InputEvent) {
+    event.preventDefault();
+
+    const inputElement = this.inputElement.nativeElement;
+    const {
+      selectionStart,
+      selectionEnd,
+      value: currentInputValue,
+    } = inputElement;
+    const data = event.data;
+
+    if (!data) {
+      return;
+    }
+
+    const newData = data.toUpperCase();
+
+    const newValue = !isNil(selectionStart)
+      ? currentInputValue.substring(0, selectionStart) +
+        newData +
+        (!isNil(selectionEnd) ? currentInputValue.substring(selectionEnd) : '')
+      : '';
+
+    inputElement.value = newValue;
+
+    const newEvent = new InputEvent('input', {
+      data: newValue,
+      inputType: event.inputType,
+      isComposing: false,
+      bubbles: true,
+      cancelable: false,
+    });
+
+    inputElement.dispatchEvent(newEvent);
   }
 }
